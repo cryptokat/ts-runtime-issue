dd391a831a76da6085f2b0a1dd32f059a99a118e
cryptokat <32390172+cryptokat@users.noreply.github.com>
2018-03-12 15:57:23 +0800

    private

--- Hash.ts
+++ Hash.ts
@@ -1,5 +1,5 @@
 export default class Hash {
-    public static h256(input : Uint8Array) : Uint8Array {
+    private h256(input : Uint8Array) : Uint8Array {
         return new Uint8Array(0);
     }
 }
